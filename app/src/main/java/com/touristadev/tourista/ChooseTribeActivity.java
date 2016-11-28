623f190e64d8eeb6d1d1acb36036bf39f014e367
Eugene Boholst <justineboholst16@gmail.com>
2016-11-28 01:54:56 -1000

    Updated Animation by Boholst

--- app/src/main/java/com/touristadev/tourista/ChooseTribeActivity.java
+++ app/src/main/java/com/touristadev/tourista/ChooseTribeActivity.java
@@ -3,32 +3,42 @@ package com.touristadev.tourista;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ToggleButton;
 
 import com.thunderrise.animations.PulseAnimation;
 
-public class ChooseTribeActivity extends AppCompatActivity implements View.OnClickListener{
+public class ChooseTribeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_tribe);
+
     }
 
-    @Override
     public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.toggle1: break;
-            case R.id.toggle2: break;
-            case R.id.toggle3: break;
-            case R.id.toggle4: break;
-            case R.id.toggle5: break;
-            case R.id.toggle6: break;
-            case R.id.toggle7: break;
-            case R.id.toggle8: break;
+        switch (v.getId()) {
+            case R.id.toggle1:
+                break;
+            case R.id.toggle2:
+                break;
+            case R.id.toggle3:
+                break;
+            case R.id.toggle4:
+                break;
+            case R.id.toggle5:
+                break;
+            case R.id.toggle6:
+                break;
+            case R.id.toggle7:
+                break;
+            case R.id.toggle8:
+                break;
         }
         PulseAnimation.create().with(v)
-                .setDuration(200)
-                .setRepeatCount(2)
+                .setDuration(100)
+                .setRepeatCount(1)
                 .setRepeatMode(PulseAnimation.REVERSE)
                 .start();
     }
