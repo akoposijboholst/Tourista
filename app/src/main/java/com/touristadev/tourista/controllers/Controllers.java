af5b0c369aff7370a2d07083882eeb8468847796
Shanyl Jimenez <falsenoglory@gmail.com>
2016-12-02 02:57:13 +0800

    populated data.

--- app/src/main/java/com/touristadev/tourista/controllers/Controllers.java
+++ app/src/main/java/com/touristadev/tourista/controllers/Controllers.java
@@ -1,5 +1,6 @@
 package com.touristadev.tourista.controllers;
 
+import com.touristadev.tourista.R;
 import com.touristadev.tourista.dataModels.Categories;
 import com.touristadev.tourista.dataModels.Itinerary;
 import com.touristadev.tourista.dataModels.Packages;
@@ -36,10 +37,12 @@ public class Controllers {
         spotList.add(new Spots(0,"Masters Resort Cebu","4044 Oslob, Cebu, Philippines",
                 "800","2100","The southern part of the province is one of the areas where you can enjoy various sorts of seawater activities, do adventurous trips, have fun and cherish the tranquil ambiance and surroundings."
                 ,"2000","9.459556960067692","123.37731275707483",
-                activities,categories,tribes,4));
+                activities,categories,tribes,4, R.mipmap.mrc));
         activities.clear();
         categories.clear();
         tribes.clear();
+
+        // SPOT ////////////////////////////////////////////////////////////////////////////////////
 
         activities.add("Whale watching");
         activities.add("Scuba Diring");
@@ -55,11 +58,13 @@ public class Controllers {
         spotList.add(new Spots(1,"Oslob Whale Shark Watching","Tan-awan Oslob, Cebu, Ph",
                 "800","1200","Oslob Whalesharks watching was born to give information about whale shark interaction in Oslob, we are here to organize day tour trips on whale shark encounters with side trip to Tumalog waterfalls. We"
                 ,"1000","9.47008451293314","123.38275127112864",
-                activities,categories,tribes,4));
+                activities,categories,tribes,4,R.mipmap.owsw));
 
         activities.clear();
         categories.clear();
         tribes.clear();
+
+        // SPOT ////////////////////////////////////////////////////////////////////////////////////
 
         activities.add("Attend Mass");
         activities.add("Visit Magellan's Cross");
@@ -75,11 +80,13 @@ public class Controllers {
         spotList.add(new Spots(2,"Basilica del Santo Niño","Santo Nino Chapel Lane, Cebu City, Cebu"
                 ,"500","2000","The oldest Roman Catholic church in the country, it is built on the spot where the image of the Santo Niño de Cebú was found during the expedition of Miguel López de Legazpi. "
                 ,"100","10.294194","123.902106",
-                activities,categories,tribes,4));
+                activities,categories,tribes,4,R.mipmap.stnino));
 
         activities.clear();
         categories.clear();
         tribes.clear();
+
+        // SPOT ////////////////////////////////////////////////////////////////////////////////////
 
         activities.add("Dicover Ancient cebu stories.");
         activities.add("Visist historical Artifacts.");
@@ -95,11 +102,13 @@ public class Controllers {
         spotList.add(new Spots(3, "Museo Sugbo", "M. J. Cuenco Ave, Cebu City, 6000 Cebu",
                 "900","1730","Museo Sugbo is the Cebu Provincial Museum located in the former Cebu Provincial Detention and Rehabilitation Center, four blocks from Plaza Independencia."
                 ,"100", "10.303781",  "123.906758",
-                activities,categories,tribes,4));
+                activities,categories,tribes,4,R.mipmap.msugbo));
 
         activities.clear();
         categories.clear();
         tribes.clear();
+
+        // SPOT ////////////////////////////////////////////////////////////////////////////////////
 
         activities.add("Explore Museum");
 
@@ -116,13 +125,55 @@ public class Controllers {
 
         spotList.add( new Spots(4,"Fort San Pedro", "A. Pigafetta Street, Cebu City, 6000"
                 ,"700", "1900", "One of the city’s historical attractions is Fort San Pedro which is known as the smallest and oldest fort in the Philippines.",
-                "100","10.292499","123.905828",activities, categories, tribes,4));
+                "100","10.292499","123.905828",activities, categories, tribes,4,R.mipmap.fsanpedro));
+
+        // SPOT ////////////////////////////////////////////////////////////////////////////////////
 
         activities.clear();
         categories.clear();
         tribes.clear();
 
-        activities.add("Ca");
+        activities.add("Swimming");
+        activities.add("Scuba Diving");
+
+        categories.add(new Categories("Adventure"));
+        categories.add(new Categories("Aquaria"));
+        categories.add(new Categories("Beaches and Resorts"));
+        categories.add(new Categories("Nature"));
+
+        tribes.add(new Tribes("Thrill-Seeker"));
+        tribes.add(new Tribes("Escapist"));
+        tribes.add(new Tribes("The Self-Improver"));
+        tribes.add(new Tribes("Genuinely Curious"));
+
+        spotList.add(new Spots(5,"Tumalog Falls","Cebu South Rd, Oslob, Cebu",
+                "500","2200","Tumalog Falls (also called the “Toslob Falls” or “Mag-ambak Falls”) is situated in the town of Oslob, Cebu. It is just about 15-20 minutes ride from the whale-watching site.",
+                "500","9.486560"," 123.369264",activities,categories,tribes,4,R.mipmap.tml));
+
+        // SPOT ////////////////////////////////////////////////////////////////////////////////////
+
+        activities.clear();
+        categories.clear();
+        tribes.clear();
+
+        activities.add("Swimming");
+
+        categories.add(new Categories("Adventure"));
+        categories.add(new Categories("Nature"));
+
+
+        tribes.add(new Tribes("Thrill-Seeker"));
+        tribes.add(new Tribes("The Self-Improver"));
+        tribes.add(new Tribes("Genuinely Curious"));
+
+        spotList.add(new Spots(6,"Kawasan Falls","Matutinao,, Badian, 6031 Cebu",
+                "500","2200","Cebu kawasan falls Cebu is a peaceful natural place where you can enjoy many waterfalls of natural spring water located near the southern tip of Cebu Philippines.. A gentle hush of rushing ice cool water."
+                , "1000","9.811219", "123.374875",activities,categories,tribes,4,R.mipmap.kws));
+
+
+        // SPOT ////////////////////////////////////////////////////////////////////////////////////
+
+
 
 
     }
@@ -185,6 +236,134 @@ public class Controllers {
         return L;
     }
 
+    public ArrayList<Packages> getControllerPackaaes()
+    {
+        Controllers();
+        ArrayList<Packages> L= new ArrayList<>();
+        ArrayList<Categories> categories = new ArrayList<>();
+        ArrayList<Tribes> tribes = new ArrayList<>();
+        ArrayList<Itinerary> itineraries = new ArrayList<>();
+
+
+        /// PACKAGE ////////////////////////////////////////////////////////////////////////////////
+
+        categories.add(new Categories("Adventure"));
+        categories.add(new Categories("Nature"));
+        categories.add(new Categories("Beaches and Resorts"));
+        categories.add(new Categories("Beaches and Resorts"));
+
+        tribes.add(new Tribes("Thrill-seeker"));
+        tribes.add(new Tribes("Backpacker"));
+        tribes.add(new Tribes("Escapist"));
+
+        itineraries.add(new Itinerary("Pick up location","500","800"));
+        itineraries.add(new Itinerary(spotList.get(1).getSpotName(),"800","1100"));
+        itineraries.add(new Itinerary("Lunch","1100","1300"));
+        itineraries.add(new Itinerary(spotList.get(0).getSpotName(),"1300","1600"));
+
+        L.add(new Packages("South Cebu Tours",categories,itineraries,"Local",tribes,4,2,8));
+
+        /// PACKAGE ////////////////////////////////////////////////////////////////////////////////
+        categories.clear();
+        tribes.clear();
+        itineraries.clear();
+
+        categories.add(new Categories("Art Galleries"));
+        categories.add(new Categories("Church"));
+        categories.add(new Categories("Historical"));
+        categories.add(new Categories("Museums"));
+
+        tribes.add(new Tribes("Collector"));
+        tribes.add(new Tribes("Escapist"));
+        tribes.add(new Tribes("The Self-Improver"));
+        tribes.add(new Tribes("Pilgrim"));
+        tribes.add(new Tribes("Genuinely Curious"));
+
+        itineraries.add(new Itinerary("Pick up location","600","900"));
+        itineraries.add(new Itinerary(spotList.get(2).getSpotName(),"900","1200"));
+        itineraries.add(new Itinerary("Lunch","1200","1300"));
+        itineraries.add(new Itinerary(spotList.get(3).getSpotAddress(),"1300","1500"));
+        itineraries.add(new Itinerary(spotList.get(4).getSpotName(),"1500","1700"));
+
+        L.add(new Packages("Cebu Pilgrimage",categories,itineraries,"Local",tribes,4,3,8));
+
+
+        /// PACKAGE ////////////////////////////////////////////////////////////////////////////////
+        categories.clear();
+        tribes.clear();
+        itineraries.clear();
+
+        categories.add(new Categories("Art Galleries"));
+        categories.add(new Categories("Church"));
+        categories.add(new Categories("Historical"));
+        categories.add(new Categories("Museums"));
+
+        tribes.add(new Tribes("Collector"));
+        tribes.add(new Tribes("Escapist"));
+        tribes.add(new Tribes("The Self-Improver"));
+        tribes.add(new Tribes("Pilgrim"));
+        tribes.add(new Tribes("Genuinely Curious"));
+
+        itineraries.add(new Itinerary("Pick up location","600","900"));
+        itineraries.add(new Itinerary(spotList.get(2).getSpotName(),"900","1200"));
+        itineraries.add(new Itinerary("Lunch","1200","1300"));
+        itineraries.add(new Itinerary(spotList.get(3).getSpotName(),"1300","1500"));
+        itineraries.add(new Itinerary(spotList.get(4).getSpotName(),"1500","1700"));
+
+        L.add(new Packages("Cebu Pilgrimage",categories,itineraries,"Local",tribes,4,3,R.mipmap.owsw));
+
+        /// PACKAGE ////////////////////////////////////////////////////////////////////////////////
+
+        categories.clear();
+        tribes.clear();
+        itineraries.clear();
+
+        categories.add(new Categories("Adventure"));
+        categories.add(new Categories("Aquaria"));
+        categories.add(new Categories("Nature"));
+
+        tribes.add(new Tribes("Thrill-Seeker"));
+        tribes.add(new Tribes("Escapist"));
+        tribes.add(new Tribes("The Self-Improver"));
+        tribes.add(new Tribes("Genuinely Curious"));
+
+        itineraries.add(new Itinerary("Pick up location","500","800"));
+        itineraries.add(new Itinerary(spotList.get(1).getSpotName(),"800","830"));
+        itineraries.add(new Itinerary("Short briefing then proceed to Whale Shark Watching / Snorkelin","830","1100"));
+        itineraries.add(new Itinerary("Lunch","1100","1200"));
+        itineraries.add(new Itinerary(spotList.get(5).getSpotName(),"1200","1500"));
+        itineraries.add(new Itinerary("Departure Time","1500","1700"));
+
+        L.add(new Packages("Oslob Whale Watching",categories,itineraries,"Local",tribes,4,2,12));
+
+        /// PACKAGE ////////////////////////////////////////////////////////////////////////////////
+
+        categories.clear();
+        tribes.clear();
+        itineraries.clear();
+
+
+
+
+
+        return L;
+    }
+
+    public ArrayList<TourRequest> getTourRequest()
+    {
+        ArrayList<TourRequest> L= new ArrayList<>();
+
+        L.add(new TourRequest("Shanyl Jimenez","December 6, 2016",2,0,"S"));
+        L.add(new TourRequest("Chan Ferolino","December 7, 2016",3,0,"S"));
+        L.add(new TourRequest("Justine Boholst","December 8, 2016",3,1,"S"));
+
+
+        return L;
+    }
+
+
+
+
     public void addBookedPackages(Packages pa)
     {
         BookedList.add(pa);
@@ -208,92 +387,6 @@ public class Controllers {
 
         return WishList;
 
-    }
-
-    public ArrayList<Packages> getControllerPackaaes()
-    {
-        Controllers();
-        ArrayList<Packages> L= new ArrayList<>();
-        ArrayList<Categories> categories = new ArrayList<>();
-        ArrayList<Tribes> tribes = new ArrayList<>();
-        ArrayList<Itinerary> itineraries = new ArrayList<>();
-
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        categories.add(new Categories("Adventure"));
-        categories.add(new Categories("Nature"));
-        categories.add(new Categories("Beaches and Resorts"));
-        categories.add(new Categories("Beaches and Resorts"));
-
-        tribes.add(new Tribes("Thrill-seeker"));
-        tribes.add(new Tribes("Backpacker"));
-        tribes.add(new Tribes("Escapist"));
-
-        itineraries.add(new Itinerary(spotList.get(1),"800","1100"));
-        itineraries.add(new Itinerary(spotList.get(0),"1300","1600"));
-
-        L.add(new Packages("South Cebu Tours",categories,itineraries,"Local",tribes,4,1500,8));
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////////
-        categories.clear();
-        tribes.clear();
-        itineraries.clear();
-
-        categories.add(new Categories("Art Galleries"));
-        categories.add(new Categories("Church"));
-        categories.add(new Categories("Historical"));
-        categories.add(new Categories("Museums"));
-
-        tribes.add(new Tribes("Collector"));
-        tribes.add(new Tribes("Escapist"));
-        tribes.add(new Tribes("The Self-Improver"));
-        tribes.add(new Tribes("Pilgrim"));
-        tribes.add(new Tribes("Genuinely Curious"));
-
-        itineraries.add(new Itinerary(spotList.get(2),"900","1200"));
-        itineraries.add(new Itinerary(spotList.get(3),"1300","1500"));
-        itineraries.add(new Itinerary(spotList.get(4),"1500","1700"));
-
-        L.add(new Packages("Cebu Pilgrimage",categories,itineraries,"Local",tribes,4,1000,8));
-
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////////
-        categories.clear();
-        tribes.clear();
-        itineraries.clear();
-
-        categories.add(new Categories("Art Galleries"));
-        categories.add(new Categories("Church"));
-        categories.add(new Categories("Historical"));
-        categories.add(new Categories("Museums"));
-
-        tribes.add(new Tribes("Collector"));
-        tribes.add(new Tribes("Escapist"));
-        tribes.add(new Tribes("The Self-Improver"));
-        tribes.add(new Tribes("Pilgrim"));
-        tribes.add(new Tribes("Genuinely Curious"));
-
-        itineraries.add(new Itinerary(spotList.get(2),"900","1200"));
-        itineraries.add(new Itinerary(spotList.get(3),"1300","1500"));
-        itineraries.add(new Itinerary(spotList.get(4),"1500","1700"));
-
-        L.add(new Packages("Cebu Pilgrimage",categories,itineraries,"Local",tribes,4,0,8));
-
-
-        return L;
-    }
-
-    public ArrayList<TourRequest> getTourRequest()
-    {
-        ArrayList<TourRequest> L= new ArrayList<>();
-
-        L.add(new TourRequest("Shanyl Jimenez","December 6, 2016",2,0,"S"));
-        L.add(new TourRequest("Chan Ferolino","December 7, 2016",3,0,"S"));
-        L.add(new TourRequest("Justine Boholst","December 8, 2016",3,1,"S"));
-
-
-        return L;
     }
 
 
