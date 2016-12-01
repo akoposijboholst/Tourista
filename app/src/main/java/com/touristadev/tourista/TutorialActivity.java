881ea5bf7a9ddafcfa381cf47d5cad2b22a0b8d4
Nick <kulastv@gmail.com>
2016-12-01 17:08:59 +0800

    UI by nicolaschiong

--- app/src/main/java/com/touristadev/tourista/TutorialActivity.java
+++ app/src/main/java/com/touristadev/tourista/TutorialActivity.java
@@ -48,9 +48,9 @@ public class TutorialActivity extends AppCompatActivity {
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.image_view_pager);
         ArrayList<String> images = new ArrayList<>();
-        images.add("http://orig05.deviantart.net/af34/f/2014/316/c/4/material_wallpaper_set_two_4_by_rafly_nxs-d865g0t.jpg");
-        images.add("http://static1.squarespace.com/static/51609147e4b0715db61d32b6/521b97cee4b05f031fd12dde/5519e33de4b06a1002802805/1431718693701/?format=1500w");
-        images.add("http://phandroid.s3.amazonaws.com/wp-content/uploads/2014/12/Ultimate-Material-Lollipop-Collection-407.png");
+        images.add("http://usjr.edu.ph/wp-content/uploads/2016/11/tutorial_discover.png");
+        images.add("http://usjr.edu.ph/wp-content/uploads/2016/11/tutorial_explore.png");
+        images.add("http://usjr.edu.ph/wp-content/uploads/2016/11/tutorial_tours.png");
         viewPager.setAdapter(new ImageAdapterTutorial(images));
         InkPageIndicator inkPageIndicator = (InkPageIndicator) findViewById(R.id.ink_pager_indicator);
         inkPageIndicator.setViewPager(viewPager);
