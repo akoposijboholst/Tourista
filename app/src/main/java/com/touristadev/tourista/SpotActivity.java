5ad9d3f92ea933f1be1a3a892eba7f481656e219
Nick <kulastv@gmail.com>
2016-12-02 23:25:18 +0800

    UI by nicolaschiong

--- app/src/main/java/com/touristadev/tourista/SpotActivity.java
+++ app/src/main/java/com/touristadev/tourista/SpotActivity.java
@@ -1,10 +1,12 @@
 package com.touristadev.tourista;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
